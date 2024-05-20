8f219f8876b978584f029194f50c593f0909ff3d
Maishan Nadis <sindeed222@gmail.com>
2024-05-20 20:18:08 +0600

    hehe

---
+++ Assignment/bellmanFord.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int INF = 1e9;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> edges;
+    for (int i = 0; i < m; i++) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        edges.push_back({u, v, w});
+    }
+    int src;
+    cin >> src;
+    vector<int> dist(n, INF);
+    dist[src] = 0;
+    for (int i = 0; i < n - 1; i++) {
+        for (auto e : edges) {
+            int u = e[0];
+            int v = e[1];
+            int w = e[2];
+            if (dist[u] != INF && dist[u] + w < dist[v]) {
+                dist[v] = dist[u] + w;
+            }
+        }
+    }
+    bool negCycle = false;
+    for (auto e : edges) {
+        int u = e[0];
+        int v = e[1];
+        int w = e[2];
+        if (dist[u] != INF && dist[u] + w < dist[v]) {
+            negCycle = true;
+            break;
+        }
+    }
+    if (negCycle) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+        for (auto d : dist) {
+            if (d == INF) {
+                cout << "INF ";
+            } else {
+                cout << d << " ";
+            }
+        }
+    }
+    return 0;
+}
